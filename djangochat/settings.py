742a28ccf09158039f92c2ecb00686c5533428e4
HeleneB <helene.blanche@hotmail.fr>
2025-12-02 16:37:41 +0100

    update settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -153,6 +153,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+CSRF_TRUSTED_ORIGINS = [
+    'https://djangochat-project.onrender.com'
+]
 
 
 
